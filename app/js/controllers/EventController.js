a8ae0ec660d13dbd689bb71dd9ca497530bbc5da
Salih, Payraw (BuffaloLZ) <payraw.salih@liazon.com>
2018-11-02 13:13:57 -0400

    update to use resources

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,9 +4,7 @@ eventsApp.controller('EventController',
     function EventController($scope, eventData){
         $scope.snippet = '<span style="color:red"> hi there </span>';
         $scope.sortOrder = "name";
-        $scope.event = eventData.getEvent(function(){
-            $scope.event = event;
-        });
+        $scope.event = eventData.getEvent();
 
         $scope.upVoteSession= function(session){
             session.upVoteCount++;
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,20 +1,7 @@
-eventsApp.factory('eventData', function ($http, $log){
-
-
-return {
-
-    getEvent: function(successcb){
-
-        $http({method: 'GET', url: '/data/event/1.json'}).
-        success(function(data,status,headers,config){
-            successcb(data);
-        }).
-        error(function (data,status,headers,config) {
-            $log.warn(data, status, headers, config);
-        });
-
-    }
-
-};
-
+eventsApp.factory('eventData', function ($resource){
+    return {
+        getEvent: function(){
+            return $resource('/data/event/:id', {id: '@id'}).get({id:1});
+        }
+    };
 });
